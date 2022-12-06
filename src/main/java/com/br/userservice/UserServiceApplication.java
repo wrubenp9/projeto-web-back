87e946f448e5f56212c7db564146bbd217536a80
Wesley Ruben <wesleyruben213@hotmail.com>
2022-12-05 22:30:16 -0300

    test deploy

--- src/main/java/com/br/userservice/UserServiceApplication.java
+++ src/main/java/com/br/userservice/UserServiceApplication.java
@@ -2,14 +2,14 @@ package com.br.userservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.context.annotation.Profile;
+import org.springframework.context.annotation.Profile;
 
-//@Profile({"dev","prod"})
 @SpringBootApplication
 public class UserServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(UserServiceApplication.class, args);
-	}
+    @Profile({"dev", "prod"})
+    public static void main(String[] args) {
+        SpringApplication.run(UserServiceApplication.class, args);
+    }
 
 }
